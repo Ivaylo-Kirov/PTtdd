61c79aaa1ec68dc3801671f06b14177dfb9fcca1
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-09-10 17:57:46 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+test class inherits from unittest.TestCase > then you have functions name with start "test" which run when test runner is called > inside those functions you have fairly standard assertion options like assertTrue, assertEqual, assertRaises(TypeError) etc.
---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+from functions import sum
+
+class TestFunctions(unittest.TestCase):
+    def test_ints(self):
+        self.assertEqual(sum(2, 2), 4)
+
+    def test_doubles(self):
+        self.assertEqual(sum(1.4, 2.2), 3.6)
+
+    def test_invalid(self):
+        with self.assertRaises(TypeError):
+            sum('hello', 2)
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        self.assertEqual('hello world'.split(), ['hello', 'world'])
+
+
+if __name__ == "__main__":
+    unittest.main()
